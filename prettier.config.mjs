0fea0e460d54d56ea44751d773758ac95a4c91ff
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-03-10 19:06:10 +0900

    add boilerplate for prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * @satisfies {import("prettier").Config}
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+const config = {
+  singleQuote: true,
+  quoteProps: 'consistent',
+};
+
+export default config;
